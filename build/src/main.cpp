663d20b238550f7b5517199ff92e616b34f0bfdb
Tylores <tylor.slay@gmail.com>
2018-12-14 14:09:59 -0800

    add the base files for all cpp projects that we uses and cleaned up some old files that were not supposed to be within the repository.

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -29,18 +29,301 @@
  *    PERFORMANCE OF THIS SOFTWARE.
 ******************************************************************************/
 
-// INCLUDES
-#include <iostream>
+// INCLUDE
+#include <iostream>     // cout, cin
+#include <thread>       // thread, join
+#include <chrono>       // now, duration
+#include <map>
+#include <string>
+#include <vector>
 
-// NAMESPACES
+#include "include/SetPoint.h"
+#include "include/xml2schedule.h"
+//#include "include/schedulizer.h"
+#include "include/tsu.h"
+#include "include/aj_utility.hpp"
+#include "include/Aggregator.hpp"
+#include "include/DistributedEnergyResource.hpp"
+#include "include/ClientListener.hpp"
+#include "include/SmartGridDevice.hpp"
+#include "include/ScheduleOperator.h"
+
 using namespace std;
+using namespace ajn;
 
-// GLOBALS
+bool done = false;  //signals program to stop
+string LOG_PATH;
+
+// Help
+// - CLI interface description
+static void Help () {
+    cout << "\n\t[Help Menu]\n\n";
+    cout << "> q                    quit\n";
+    cout << "> h                    display help menu\n";
+    cout << "> a                    print all resources\n";
+    cout << "> f <arg arg ...>      print target resources\n";
+    cout << "> F                    print target resources\n";
+    cout << "> t                    print resource totals\n";
+    cout << "> e <watts>            send export signal to target resources\n";
+    cout << "> i <watts>            send import signal to target resources\n";
+} // end Help
+
+// Command Line Interface
+// - method to allow user controls during program run-time
+static bool CommandLineInterface (const string& input, Aggregator* vpp) {
+    // check for program argument
+    if (input == "") {
+        return false;
+    }
+    char cmd = input[0];
+
+    // deliminate input string to argument parameters
+    vector <string> tokens;
+    stringstream ss(input);
+    string token;
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    switch (cmd) {
+        case 'q':
+           return true;
+
+        case 'a': {
+            vpp->DisplayAllResources ();
+	    return false;
+        }
+
+    case 'f': {
+        tokens.erase(tokens.begin());  // remove CLI command
+        vpp->SetTargets (tokens);
+        return false;
+    }
+
+	case 'F': {
+	    vpp->DisplayTargetResources ();
+	    return false;
+	}
+
+	case 't': {
+	    vpp->DisplayTotals ();
+	    return false;
+	}
+
+	case 'e': {
+	    try {
+		vpp->ExportPower(stoul(tokens[1]));
+	    } catch(...) {
+		cout << "[ERROR]: Invalid Argument." << endl;
+	    }
+	    return false;
+	}
+
+	case 'i': {
+	    try {
+		vpp->ImportPower(stoul(tokens[1]));
+	    } catch(...) {
+		cout << "[ERROR]: Invalid Argument." << endl;
+	    }
+	    return false;
+	}
+
+        default: {
+            Help();
+	    return false;
+        }
+    }
+}  // end Command Line Interface
+
+void AggregatorLoop (Aggregator *VPP) {
+    unsigned int time_remaining, time_past;
+    unsigned int time_wait = 500;
+    auto time_start = chrono::high_resolution_clock::now ();
+    auto time_end = chrono::high_resolution_clock::now ();
+    chrono::duration <double, milli> time_elapsed;
+
+    while (!done) {
+        time_start = chrono::high_resolution_clock::now();
+            // time since last control call;
+            time_elapsed = time_start - time_end;
+            time_past = time_elapsed.count();
+            VPP->Loop(time_past);
+        time_end = chrono::high_resolution_clock::now();
+        time_elapsed = time_end - time_start;
+
+        // determine sleep duration after deducting process time
+        time_remaining = (time_wait - time_elapsed.count());
+        time_remaining = (time_remaining > 0) ? time_remaining : 0;
+        this_thread::sleep_for (chrono::milliseconds (time_remaining));
+    }
+}  // end Aggregator Loop
+
+
+void OperLoop (ScheduleOperator *Oper) {
+	unsigned int time_remaining, time_past;
+    unsigned int time_wait = 1000;
+    auto time_start = chrono::high_resolution_clock::now();
+    auto time_end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> time_elapsed;
+
+    while (!done) {
+        time_start = chrono::high_resolution_clock::now();
+            // time since last control call;
+            time_elapsed = time_start - time_end;
+            time_past = time_elapsed.count();
+            Oper->Loop();
+        time_end = chrono::high_resolution_clock::now();
+        time_elapsed = time_end - time_start;
+
+        // determine sleep duration after deducting process time
+        time_remaining = (time_wait - time_elapsed.count());
+        time_remaining = (time_remaining > 0) ? time_remaining : 0;
+        this_thread::sleep_for (chrono::milliseconds (time_remaining));
+    }
+}  // end Operator Loop
+
+
 
 
 // Main
 // ----
 int main (int argc, char** argv) {
     cout << "\nStarting Program...\n";
-    return 0;
+    cout << "\tMapping configuration file...\n";
+    // Tylor: you might want to change this later. - Kevin
+    tsu::config_map ini_map = tsu::MapConfigFile("../DERAS/data/config.ini");
+
+    // (TS): I set this to global because I can't think of a good way to make it
+    // available to all files.
+    LOG_PATH = ini_map["Logging"]["path"];
+
+    cout << "\tStarting AllJoyn...\n";
+    try {
+        AllJoynInit();
+    } catch (exception &e) {
+        cout << "[ERROR]: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+
+    #ifdef ROUTER
+        try {
+            AllJoynRouterInit();
+        } catch (exception &e) {
+            cout << "[ERROR]: " << e.what() << endl;
+            return EXIT_FAILURE;
+        }
+    #endif // ROUTER
+
+    cout << "\t\tCreating message bus...\n";
+    const char* app_name = ini_map["AllJoyn"]["app"].c_str();
+    bool allow_remote = true;
+    BusAttachment *bus_ptr = new BusAttachment(app_name, allow_remote);
+    assert(bus_ptr != NULL);
+
+    cout << "\t\tCreating about object...\n";
+    AboutData about_data("en");
+    AboutObj *about_ptr = new AboutObj(*bus_ptr);
+    assert(about_ptr != NULL);
+
+    cout << "\t\tEstablishing session port...\n";
+    aj_utility::SessionPortListener SPL;
+    ajn::SessionPort port = stoul (ini_map["AllJoyn"]["port"]);
+
+    cout << "\t\tSetting up bus attachment...\n";
+    QStatus status = aj_utility::SetupBusAttachment (ini_map,
+                                                     port,
+                                                     SPL,
+                                                     bus_ptr,
+                                                     &about_data);
+
+    if (status != ER_OK) {
+        delete about_ptr;
+        delete bus_ptr;
+        return EXIT_FAILURE;
+    }
+
+    cout << "\t\tCreating observer...\n";
+    const char* client_name = ini_map["AllJoyn"]["client_interface"].c_str();
+    Observer *obs_ptr = new Observer(*bus_ptr, &client_name, 1);
+
+    cout << "\t\tCreating virtual power plant...\n";
+    Aggregator *vpp_ptr = new Aggregator (ini_map);
+
+    cout << "\t\tCreating listener...\n";
+    ClientListener *listner_ptr = new ClientListener(bus_ptr,
+                                                     obs_ptr,
+                                                     vpp_ptr,
+                                                     client_name);
+    obs_ptr->RegisterListener(*listner_ptr);
+
+    cout << "\t\tCreating bus object...\n";
+    const char* server_name = ini_map["AllJoyn"]["server_interface"].c_str();
+    const char* path = ini_map["AllJoyn"]["path"].c_str();
+    SmartGridDevice *sgd_ptr = new SmartGridDevice(bus_ptr,
+                                                   server_name,
+                                                   path);
+
+    cout << "\t\t\tRegistering bus object...\n";
+    if (ER_OK != bus_ptr->RegisterBusObject(*sgd_ptr)){
+        printf("[ERROR] failed registration...\n");
+        delete &sgd_ptr;
+        return EXIT_FAILURE;
+    }
+    about_ptr->Announce(port, about_data);
+
+    cout << "Program initialization complete...\n";
+
+    thread VPP (AggregatorLoop, vpp_ptr);
+
+
+// Beginning of Inserted Operator Code
+
+    //Schedulizer("../DERAS/data/timeActExt.csv")
+
+    std::vector<SetPoint> schedule = xml2schedule("../DERAS/data/EIM.xml");
+
+
+    //ScheduleOperator *opr_ptr = new ScheduleOperator("../DERAS/data/timeActExt.csv", vpp_ptr);
+	ScheduleOperator *opr_ptr = new ScheduleOperator(schedule, vpp_ptr);
+	thread Oper(OperLoop, opr_ptr); //Same as control loop, except EWH->Loop(), Oper->Loop()
+
+
+//       End of Inserted Operator Code.
+
+
+
+
+
+    Help ();
+    string input;
+
+    while (!done) {
+        getline(cin, input);
+        done = CommandLineInterface(input, vpp_ptr);
+    }
+
+    cout << "Program shutting down...\n";
+    cout << "\t Joining threads...\n";
+    VPP.join ();
+	Oper.join();  //Added by Kevin.
+                  //Waits for the loop to close, and then closes the thread.
+
+    cout << "\t deleting pointers...\n";
+    delete sgd_ptr;
+    delete listner_ptr;
+    delete vpp_ptr;
+    delete obs_ptr;
+    delete about_ptr;
+    delete bus_ptr;
+
+    cout << "\t Shutting down AllJoyn...\n";
+    obs_ptr->UnregisterAllListeners ();
+
+    #ifdef ROUTER
+        AllJoynRouterShutdown ();
+    #endif // ROUTER
+
+    AllJoynShutdown ();
+
+    return EXIT_SUCCESS;
 } // end main
--- tools/build-run.sh
+++ tools/build-run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 # setup environment
+export CPU=x86_64
+export OS=linux
+export VARIANT=debug
+export AJ_ROOT=$HOME/src/core-alljoyn/build/$OS/$CPU/$VARIANT/dist/cpp
+export AJ_LIB=$AJ_ROOT/lib
+export AJ_INC=$AJ_ROOT/inc
+export LD_LIBRARY_PATH=$AJ_LIB:$LD_LIBRARY_PATH
+
+# alljoyn router
+# first kill any daemon's that are currently running, then start new daemon
+# the last /dev/null 2>&1 & has all stdout set to null and runs in background
+export AJ_ROUTER=stand-alone
+killall alljoyn-daemon
+sleep 1
+$AJ_CPP/bin/alljoyn-daemon --config-file=../data/rn-config.xml > /dev/null 2>&1 &
 
 # build
 export SRC=deras
